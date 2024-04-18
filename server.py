cb06637266b35a22522ba39d33614e7d44a5c4e2
yassamanmajedi <166200730+yassamanmajedi@users.noreply.github.com>
2024-04-18 14:20:04 +0330

    Add files via upload

---
+++ server.py
@@ -0,0 +1,77 @@
+# imports
+import socket
+import threading
+
+
+class ChatServer:
+
+    clients_list = []
+
+    last_received_message = ""
+
+    def __init__(self):
+        self.server_socket = None
+        self.create_listening_server()
+    # listen for incoming connection
+
+    def create_listening_server(self):
+
+        # create a socket using TCP port and ipv4
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        local_ip = '127.0.0.1'
+        local_port = 10319
+        # this will allow you to immediately restart a TCP server
+        self.server_socket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # this makes the server listen to requests coming from other computers on the network
+        self.server_socket.bind((local_ip, local_port))
+        print("Listening for incoming messages..")
+        # listen for incomming connections / max 5 clients
+        self.server_socket.listen(5)
+        self.receive_messages_in_a_new_thread()
+    # fun to receive new msgs
+
+    def receive_messages(self, so):
+        while True:
+            incoming_buffer = so.recv(2048)  # initialize the buffer
+            if not incoming_buffer:
+                break
+            self.last_received_message = incoming_buffer.decode('utf-8')
+            if 'START_OF_FILE' in self.last_received_message:
+                f = open('test.txt', 'wb')  # open in binary
+                while True:
+                    chunk = so.recv(2048)
+                    try:
+                        if 'END_OF_FILE' in chunk.decode('utf-8'):
+                            f.close()
+                            break
+                    except:
+                        pass
+                    f.write(chunk)
+            else:
+                self.broadcast_to_all_clients(so)  # send to all clients
+        so.close()
+    # broadcast the message to all clients
+
+    def broadcast_to_all_clients(self, senders_socket):
+        for client in self.clients_list:
+            socket, (ip, port) = client
+            if socket is not senders_socket:
+                socket.sendall(self.last_received_message.encode('utf-8'))
+
+    def receive_messages_in_a_new_thread(self):
+        while True:
+            client = so, (ip, port) = self.server_socket.accept()
+            self.add_to_clients_list(client)
+            print('Connected to ', ip, ':', str(port))
+            t = threading.Thread(target=self.receive_messages, args=(so,))
+            t.start()
+    # add a new client
+
+    def add_to_clients_list(self, client):
+        if client not in self.clients_list:
+            self.clients_list.append(client)
+
+
+if __name__ == "__main__":
+    ChatServer()
